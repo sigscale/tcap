f1deec2e587f49440f3361a5113a8031f3066404
Harald Welte <laforge@gnumonks.org>
2013-06-18 12:34:16 +0200

    edoc doesn't support multiple authors in one @author tag

--- src/ITU/tcap_tco_server.erl
+++ src/ITU/tcap_tco_server.erl
@@ -1,7 +1,8 @@
 %%% $Id: tcap_tco_server.erl,v 1.7 2005/08/04 09:33:17 vances Exp $
 %%%---------------------------------------------------------------------
 %%% @copyright 2004-2005 Motivity Telecom, 2010-2011 Harald Welte
-%%% @author Vance Shipley <vances@motivity.ca>, Harald Welte <laforge@gnumonks.org>
+%%% @author Vance Shipley <vances@motivity.ca>
+%%% @author Harald Welte <laforge@gnumonks.org>
 %%% @end
 %%%
 %%% Copyright (c) 2004-2005, Motivity Telecom
